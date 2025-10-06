52fe90231f2570a700fc9d91302114184c92143a
Khushidev24 <khushidev24@gmail.com>
2025-10-06 12:41:18 +0530

    Update test_calculator.py

--- tests/unit/test_calculator.py
+++ tests/unit/test_calculator.py
@@ -3,8 +3,8 @@ Unit Tests for Calculator
 Students start with 2 passing tests, then add more
 """
 import pytest
-from src.calculator import add, divide, subtract
-from calculator import multiply
+from src.calculator import add, divide, subtract,multiply
+
 
 class TestBasicOperations:
     """Test basic arithmetic operations"""
